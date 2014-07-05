e9173589f9b503283b8b332a6ab60bfc3bda5ee6
Kevin Casey <kcasey@godaddy.com>
2014-07-05 13:05:45 -0700

    hi

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,7 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
+    p 'create'
     @user = User.new(user_params)
 
     respond_to do |format|
@@ -69,7 +70,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      p params
       params.require(:user).permit(:name, :phone, :email, :password, :password_confirmation)
     end
 end
